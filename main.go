6e47483966a2a61efc50191a2b247149163cce48
Michel Blanc <mb@devops.works>
2020-03-31 13:09:09 +0200

    Adds SS coordinates

--- main.go
+++ main.go
@@ -16,9 +16,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var casesURL = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv"
-
-// var deathsURL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+var casesURL = "https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/"
 
 type country struct {
 	points    []point
